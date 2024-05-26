d9caf5ca1ba3841331659abbb4d865a78b70c9e0
BhaveshKapre19 <kapre19091999@gmail.com>
2024-05-26 13:16:43 +0530

    chart added

--- budget/urls.py
+++ budget/urls.py
@@ -1,7 +1,7 @@
 # budget/urls.py
 
 from django.urls import path
-from .views import HomeView, RegisterView, TransactionListView, TransactionCreateView, TransactionUpdateView, TransactionDeleteView
+from .views import ExportPDFView,HomeView, RegisterView, TransactionListView, TransactionCreateView, TransactionUpdateView, TransactionDeleteView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('transaction/new/', TransactionCreateView.as_view(), name='transaction-create'),
     path('transaction/<int:pk>/edit/', TransactionUpdateView.as_view(), name='transaction-edit'),
     path('transaction/<int:pk>/delete/', TransactionDeleteView.as_view(), name='transaction-delete'),
+    path('transactions/export_pdf/', ExportPDFView.as_view(), name='export_pdf'),
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,14 +1,19 @@
+# budget/views.py
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView,TemplateView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, TemplateView, View
 from .forms import UserRegisterForm, TransactionForm
 from .models import Transaction
 from django.contrib.auth import login as auth_login
 from django.db.models import Sum
 from django.utils import timezone
 from datetime import timedelta
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'budget/home.html'
@@ -16,20 +21,61 @@ class HomeView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Calculate total amount spent in the last 30 days
+        user = self.request.user
+        
+        # User Info
+        context['username'] = user.username
+        context['email'] = user.email
+
+        # Calculate total amount spent and earned in the last 30 days
         thirty_days_ago = timezone.now() - timedelta(days=30)
         total_spent_last_30_days = Transaction.objects.filter(
-            user=self.request.user,
-            date__gte=thirty_days_ago
+            user=user,
+            date__gte=thirty_days_ago,
+            type='expense'
         ).aggregate(Sum('amount'))['amount__sum'] or 0
+
+        total_income_last_30_days = Transaction.objects.filter(
+            user=user,
+            date__gte=thirty_days_ago,
+            type='income'
+        ).aggregate(Sum('amount'))['amount__sum'] or 0
+
         context['total_spent_last_30_days'] = total_spent_last_30_days
+        context['total_income_last_30_days'] = total_income_last_30_days
+
         # Get recent transactions
         recent_transactions = Transaction.objects.filter(
-            user=self.request.user
+            user=user
         ).order_by('-date')[:5]  # Get the latest 5 transactions
+
         context['recent_transactions'] = recent_transactions
+
+        # Filtered Transactions
+        filter_type = self.request.GET.get('filter')
+        transactions = Transaction.objects.filter(user=user)
+        if filter_type:
+            transactions = transactions.filter(type=filter_type)
+        context['transactions'] = transactions
+
         return context
 
+class ExportPDFView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        transactions = Transaction.objects.filter(user=user)
+        template_path = 'budget/export_pdf.html'
+        context = {'transactions': transactions, 'user': user}
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="transactions.pdf"'
+        template = get_template(template_path)
+        html = template.render(context)
+        pisa_status = pisa.CreatePDF(
+            html, dest=response
+        )
+        if pisa_status.err:
+            return HttpResponse('We had some errors <pre>' + html + '</pre>')
+        return response
 
 class RegisterView(CreateView):
     form_class = UserRegisterForm
